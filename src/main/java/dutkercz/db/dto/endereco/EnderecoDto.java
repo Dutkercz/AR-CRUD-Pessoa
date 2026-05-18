524ead0846de62190c8ab4ac0da228db2f410f0e
Dutkercz <dutkercz@gmail.com>
2026-05-18 11:27:10 -0300

    feat: adicionado tratamento de exceções em GlobalExceptionHandler.java e validações nos campos de EnderecoDto

--- src/main/java/dutkercz/db/dto/endereco/EnderecoDto.java
+++ src/main/java/dutkercz/db/dto/endereco/EnderecoDto.java
@@ -1,9 +1,20 @@
 package dutkercz.db.dto.endereco;
 
-public record EnderecoDto(String rua,
-                          String numero,
-                          String bairro,
-                          String cidade,
-                          String estado,
-                          String cep) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record EnderecoDto(
+        @NotBlank(message = "O campo rua não pode estar em branco")
+        String rua,
+        @NotBlank(message = "O campo número não pode estar em branco")
+        String numero,
+        @NotBlank(message = "O campo bairro não pode estar em branco")
+        String bairro,
+        @NotBlank(message = "O campo cidade não pode estar em branco")
+        String cidade,
+        @NotBlank(message = "O campo estado não pode estar em branco")
+        String estado,
+        @NotBlank(message = "O campo cep não pode estar em branco")
+        @Pattern(regexp = "^\\d{5}-?\\d{3}$", message = "O campo cep está fora do formato esperado")
+        String cep) {
 }
---
+++ src/main/java/dutkercz/db/exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package dutkercz.db.exception;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+
+        e.getFieldErrors().forEach(error -> errors.put(
+                error.getField(),
+                error.getDefaultMessage())
+        );
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    ///Apenas o campo CPF foi marcado com UNIQUE
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<?> handleDataIntegrityViolation(DataIntegrityViolationException e) {
+        String message;
+        if (e.getMostSpecificCause().getMessage().contains("cpf")) {
+            message = "O cpf informado já possui cadastro";
+        }else {
+            message = "Erro inesperado";
+        }
+        return ResponseEntity.badRequest().body(Map.of("message: ", message));
+    }
+}
