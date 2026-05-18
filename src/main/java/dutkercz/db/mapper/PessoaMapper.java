04354115982cd00e4cf22a5f941d8f023bd6cffe
Dutkercz <dutkercz@gmail.com>
2026-05-18 09:41:55 -0300

    fix: ajustando o Mapper de Pessoa para fazer a persistência correta de endereços

--- src/main/java/dutkercz/db/mapper/PessoaMapper.java
+++ src/main/java/dutkercz/db/mapper/PessoaMapper.java
@@ -5,12 +5,24 @@ import dutkercz.db.domain.Pessoa;
 import dutkercz.db.dto.endereco.EnderecoDto;
 import dutkercz.db.dto.pessoa.PessoaRequestDto;
 import dutkercz.db.dto.pessoa.PessoaResponseDto;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface PessoaMapper {
 
     Pessoa toEntity(PessoaRequestDto requestDto);
+
+    @AfterMapping
+    default void setPessoaToEndereco(@MappingTarget Pessoa pessoa){
+        if (pessoa.getEnderecos() != null){
+            for (Endereco endereco : pessoa.getEnderecos()) {
+                endereco.setPessoa(pessoa);
+            }
+        }
+    }
+
     PessoaResponseDto toDto(Pessoa pessoa);
 
     ///Utilitario para mapeamento da entidade de composição
